709e077e36f07d8a3ef959e64f28e2c414ba4fb8
Ananxv <82608189+Ananxv@users.noreply.github.com>
2022-12-13 19:38:23 +0000

    Update auxiliar_player.h
    auxiliary function read_file added

--- client/auxiliar_player.h
+++ client/auxiliar_player.h
@@ -19,6 +19,7 @@ ssize_t read_from_TCP_socket(char *buffer, ssize_t nleft);
 void start(char* hostname, char* port, char *buffer, char *PLID, char *game, int *trial_number);
 void play(char* hostname, char* port, char *buffer, char *PLID, char *game, int *trial_number);
 void guess(char* hostname, char* port, char *buffer, char *PLID, int *trial_number);
+void read_file(int print, char *ptr, char *buffer);
 void scoreboard(char* hostname, char* port, char *buffer, char *PLID);
 void hint(char* hostname, char* port, char *buffer, char *PLID);
 void state(char* hostname, char* port, char *buffer, char *PLID);
